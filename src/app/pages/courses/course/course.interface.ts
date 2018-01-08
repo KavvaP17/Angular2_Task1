9231a6b2bd94bd2a4b048fa8ea744d08d8ff2bff
Александр <KavvaP17@list.ru>
2018-01-08 15:14:38 +0300

    Update course.interface.ts

--- src/app/pages/courses/course/course.interface.ts
+++ src/app/pages/courses/course/course.interface.ts
@@ -1,6 +1,9 @@
 interface Course {
+    id: number;
     title: string;
     duration: number;
     date: number;
     description: string;
+    edit: (title: string, duration: number, date: number, description: string) => Course;
+    delete: (id: number) => void;
 }
